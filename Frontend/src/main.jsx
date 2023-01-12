89562f6e8091bd2310dc79f065b6e1d57fc7d2be
HabibDi <habibdiallo78@gmail.com>
2023-01-12 10:02:14 +0100

    adds Login form

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,24 +2,28 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./App.css";
+
+//import components
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
+//import pages
 import Home from "./pages/Home";
+import Signup from "./pages/Signup"
+import Login from "./pages/Login"
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
   },
-  // {
-  //   path: "/addatoy",
-  //   element: <AddAToy />,
-  // },
-  // {
-  //   path: "/toydetails/:id",
-  //   element: <ToyDetails />,
-  // },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
